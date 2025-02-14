0dde143dc728623c0682374a7fca3af2d04062d6
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2025-02-14 14:36:32 +0100

    Remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use slint::{Model, ModelRc, VecModel};
 use std::error::Error;
-use std::rc::Rc;
-
-use slint::SharedString;
 
 slint::include_modules!();
 
